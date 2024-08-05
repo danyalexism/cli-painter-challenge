3d8cdf1f53086cf23d61d3516fafd83e15317b57
Dany <danyaleximoru@gmail.com>
2024-08-05 13:30:57 -0500

    trabajo en clase

--- painter/model.py
+++ painter/model.py
@@ -1,1 +1,30 @@
-# TODO: Add code here
+
+import matplotlib.pyplot as plt
+import pickle
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = 0
+        self.y = 0
+pt1 = Point()
+pt2 = Point()
+
+class Circle:
+pi = 3.1416
+
+    def __init__(self, center, radius):
+      self.center = center
+      self.radius = radius
+    def area (self):
+        return pi * self.radius ^2
+    def draw (self):
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
+    def _str_ (x,y):
+        Circle with center at (x, y) and radius r
+
+class Triangle:
+
